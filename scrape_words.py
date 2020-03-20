d4b0f2939f57ed04940bd7687e4a71d777043aa4
Matt <=>
2020-03-21 00:28:21 +1100

    still need to make scraper function standalone

--- scrape_words.py
+++ scrape_words.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
-from random import randint
+import pdb
 import lxml
 import requests
 import json
+from PasswordGenerator import PasswordGenerator
 
 
 word_lists = {
@@ -50,21 +51,17 @@ def generate_lists(l):
         
         entry['list'] = word_scraper(list_type=ltype, url=url)
 
-def randListItem(l):
-    return l[randint(0, len(l)-1)]
-
-def generatePassword(noun, adj):
-    return randListItem(adj) + randListItem(noun) + str(randint(10, 99))
-
 def main():
     generate_lists(word_lists)
     with open('result.json', 'w') as fp:
         json.dump(word_lists, fp, indent=4)
     
-    noun = word_lists['types'][0]['list']
-    adj = word_lists['types'][1]['list']
-    print(generatePassword(noun, adj))
+    #pdb.set_trace()
 
+    pgen = PasswordGenerator(nounlist=word_lists['types'][0]['list'], adjlist=word_lists['types'][1]['list'], difficulty=3)
+    print("length = " + str(len(pgen)))
+    print(pgen.generateBasicPassword())
+    print(pgen.generateBasicPassword())
 
 if __name__ == '__main__':
     main()
